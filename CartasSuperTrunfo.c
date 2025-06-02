b0b9624bdc0d0f29e9765ea41e1e8397e35a8ba6
Lorrainec-dev <devlorraine@outlook.com>
2025-06-02 14:49:21 +0000

    carta 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,47 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+
+    char estado [20]; 
+    char codigo[20];
+    char cidade [20];
+    int populacao;
+    int PontosTuristicos;
+    float PIB;
+    float area;
+
+    printf("Digite o Estado:\n");
+    scanf("%s", &estado);
+
+    printf("Digite o Código:\n");
+    scanf("%s", &codigo);
+
+    printf("Digite o Nome da Cidade:\n");
+    scanf("%s", &cidade);
+
+    printf("População total:\n");
+    scanf("%d", &populacao);
+
+    printf("Quantidade de Pontos Turísticos:\n");
+    scanf("%d", &PontosTuristicos);
+
+    printf("Área total:\n");
+    scanf("%f", &area);
+
+    printf("PIB\n");
+    scanf("%f", &PIB);
+
+
+
+    printf("Carta 1:\n");
+    printf("Estado: %s \n", estado);
+    printf("Código da Carta: %s \n" ,codigo);
+    printf("Cidade: %s \n", cidade);
+    printf("População: %d \n", populacao);
+    printf("Pontos Turisticos: %d \n" , PontosTuristicos);
+    printf("Área total: %f \n", area);
+    printf("PIB: %f \n", PIB);
+
 
     return 0;
 }
